891b460c6540bef8f54490d11bf43cfcbbb0b370
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2022-04-16 19:44:19 -0400

    convert to dataclass

--- netnutrition.py
+++ netnutrition.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 from helpers import grab_id_from_parens
+from dataclasses import dataclass
 
 class NetNutrition:
 	"""A class representing the net nutrition API and keeping track of the current state?
@@ -7,14 +8,18 @@ class NetNutrition:
 	pass
 
 
+@dataclass
 class DiningLocation:
+	name:str
+	identifier: int
+
 	@classmethod
 	def from_html(cls, html):
-		link = html.find(id="cbo_nn_unitNameLink")
+		link = html.find(class_="cbo_nn_unitNameLink")
 		name = link.string
 		identifier = grab_id_from_parens(link["onclick"])
 		return cls(name, identifier)
 
-	def __init__(self, name, identifier):
-		self.name = name
-		self.id = identifier
+	# def __init__(self, name, identifier):
+	# 	self.name = name
+	# 	self.id = identifier
