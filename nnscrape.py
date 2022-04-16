be3a36f7dfb6f6a067ea5d33bc629d502d3d75cb
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2022-04-16 18:36:12 -0400

    pipfile and starter python file

---
+++ nnscrape.py
@@ -0,0 +1,7 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+
+NN_BASE_URL = "https://www.rit.edu/fa/diningservices/netnutrition/1"
+# https://www.rit.edu/fa/diningservices/netnutrition/1/NutritionDetail/ShowItemNutritionLabel
