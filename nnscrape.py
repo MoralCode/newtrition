83cc5496667a48f031b65d7b162ea8c9791b7668
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2022-04-16 19:12:05 -0400

    persist cookies to file

--- nnscrape.py
+++ nnscrape.py
@@ -1,11 +1,21 @@
 from bs4 import BeautifulSoup
 import requests
+import pickle
+from pathlib import Path
 
 
-
+COOKIES_FILE = "cookies.txt"
 NN_BASE_URL = "https://www.rit.edu/fa/diningservices/netnutrition/1"
 # https://www.rit.edu/fa/diningservices/netnutrition/1/NutritionDetail/ShowItemNutritionLabel
 
 session = requests.Session() 
+if Path(COOKIES_FILE).exists():
+	with open(COOKIES_FILE, 'rb') as c:
+		# contents = c.read()
+		# if len(contents) > 0:
+		session.cookies.update(pickle.load(c))
 
 homepage = session.get(NN_BASE_URL)
+
+with open(COOKIES_FILE, 'wb') as c:
+	pickle.dump(session.cookies, c)
