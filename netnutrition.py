b9e2f2ad1717e722b074ec4439de00d55430d9d8
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2022-04-16 20:22:31 -0400

    set up bidirectional references between a location and a menu

--- netnutrition.py
+++ netnutrition.py
@@ -13,6 +13,30 @@ class DiningLocation:
 	name:str
 	identifier: int
 
+	@property
+	def menus(self):
+		if self._menus is not None:
+			return self.menus
+		
+		menus_url = NN_BASE_URL + "/Unit/SelectUnitFromUnitsList"
+		menu_data = "unitOid=" + str(artesanos.identifier)
+		menu_headers = {
+			"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+			# "Accept": "*/*",
+			"Referer": NN_BASE_URL
+		}
+
+		menu_response = session.post(menus_url, data=menu_data, headers=menu_headers)
+		menu_panels = menu_response.json().get("panels")
+		menu_panel = next((x for x in menu_panels if x.get("id") == "menuPanel"), None)
+		if not menu_panel:
+			print("cannot find menu html")
+		menu_html = menu_panel.get("html")
+		menu_list_html = BeautifulSoup(menu_html, 'html.parser') 
+		menu_list_items = menu_list_html.find(id="cbo_nn_menuDataList").find(class_="row").children
+		self._menus = [DiningMenu.from_html(html, for_location=self) for html in menu_list_items]
+		return self._menus
+
 	@classmethod
 	def from_html(cls, html):
 		link = html.find(class_="cbo_nn_unitNameLink")
@@ -29,9 +53,20 @@ class DiningMenu:
 	date:str
 	identifier:int
 
+	@property
+	def location(self):
+		return self._location
+	
+	@property.setter
+	def location(self, location):
+		self._location = location
+
 	@classmethod
-	def from_html(cls, html):
+	def from_html(cls, html, for_location=None):
 		title = html.find(class_="card-title")
 		date = title.string
 		identifier = grab_id_from_parens(html.find(class_="cbo_nn_menuLink")["onclick"])
-		return cls(date, identifier)
+		ins = cls(date, identifier)
+		if for_location:
+			ins.location = for_location
+		return ins
