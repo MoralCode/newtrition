3093ede3de9784ce0745f58c6f550cf89b7d0b95
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2022-04-17 17:05:35 -0400

    create initial revision

---
+++ migrations/versions/69029c692190_initial_version.py
@@ -0,0 +1,24 @@
+"""initial version
+
+Revision ID: 69029c692190
+Revises: 
+Create Date: 2022-04-17 17:05:21.956696
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '69029c692190'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
